9a23a82d7405e0ca0b3bb71437da8b3a88486c79
AnudeeP-1 <62413866+AnudeeP-1@users.noreply.github.com>
2024-11-05 17:09:16 +0530

    Update FunRestController.java
    Changed sop

--- src/main/java/com/anudeep/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/anudeep/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -14,6 +14,7 @@ public class FunRestController {
     }
     @GetMapping("/")
     public String homePage(){
-        return "Hello, I'm hosted in github!";
+        return "Hello, I'm hosted in github comminting to check in jenkins!";
     }
+    
 }
